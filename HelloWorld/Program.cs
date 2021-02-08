61d8053fefb9f26756f8694905cb19f02bfd2027
Bruno Koga <brunokoga187@gmail.com>
2021-02-08 16:27:32 -0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um valor de 1 a 10!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/obj/NameSpaceTest/HelloWorldAlternative.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld.NameSpaceTest
+{
+    public class HelloWorldAlternative
+    {
+        // Bellow the short version
+        // static void Main(string[] args) => Console.WriteLine("Hello World!");
+
+        //Bellow the full old version
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
